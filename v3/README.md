2fd67267b5c9e0ac79b953a51f8cbdacb0e585fb
macimas <macimas@vivaldi.net>
2024-05-03 22:54:27 +0800

    quick show, still wip

---
+++ v3/README.md
@@ -0,0 +1,17 @@
+my personal css stuff for [v3](https://vorapis.pages.dev)
+
+### semi-better theater mode.css
+an attempt to make theater mode act more like in modern YouTube
+<br>
+somewhat tweakable with some variables in `:root`
+
+it also works with StarTube at least when i tried it with most layouts, altho i don't really use it `:p`
+
+![preview](semi-better_theater_mode.gif)
+
+### semi-better vordebugger.css
+an attempt to improve the vordebugger, with my personal preference
+<br>
+it's a bit iffy, but should be good enough (for me at the very least)
+
+![preview](semi-better_vordebugger.png)
---
+++ v3/toggle theater with key.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         toggle theater with key
+// @namespace    https://github.com/macimas
+// @version      1.0
+// @description  toggle theater with key. yeah!
+// @author       macimas (https://macimas.github.io)
+// @match        www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener("keydown", event => {
+    const theater_button = document.querySelector("div.ytp-size-toggle-small, div.ytp-size-toggle-large");
+    const is_upsell = document.querySelector("#upsell-video .ytp-size-toggle-large");
+
+    if (theater_button && event.code == "KeyT") {
+        if (is_upsell) {
+            alert("hello! this is the channel trailer speaking! thank you for pressing the theater key! as a reward, you get a pointless button for your work :D")
+        }
+        theater_button.click();
+    }
+});
